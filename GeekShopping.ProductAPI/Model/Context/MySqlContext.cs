067a0c30e16fb7c8d0e066ca86f489b0dbd6de4d
AndreCardoso02 <andremirandacardoso02@gmail.com>
2023-08-29 14:19:43 +0100

    Adicionando a Model de Productos

--- GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
+++ GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
@@ -4,7 +4,14 @@ namespace GeekShopping.ProductAPI.Model.Context
 {
     public class MySqlContext : DbContext
     {
+        #region ctor
         public MySqlContext() {}
         public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) {}
+        #endregion
+
+        #region DbSets
+        public DbSet<Product> Products { get; set; }
+        #endregion
+
     }
 }
