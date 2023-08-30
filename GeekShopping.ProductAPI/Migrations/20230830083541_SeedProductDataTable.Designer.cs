234da095d43fea235eae7843eb1ff701410379c0
AndreCardoso02 <andremirandacardoso02@gmail.com>
2023-08-30 09:39:25 +0100

    Adicionando Seeds para inserção de dados

--- GeekShopping.ProductAPI/Migrations/20230830083541_SeedProductDataTable.Designer.cs
+++ GeekShopping.ProductAPI/Migrations/20230830083541_SeedProductDataTable.Designer.cs
@@ -2,6 +2,7 @@
 using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GeekShopping.ProductAPI.Migrations
 {
     [DbContext(typeof(MySqlContext))]
-    partial class MySqlContextModelSnapshot : ModelSnapshot
+    [Migration("20230830083541_SeedProductDataTable")]
+    partial class SeedProductDataTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
