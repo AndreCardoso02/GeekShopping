2723974813b8735765dce3bd6764799f7ae581a3
AndreCardoso02 <andremirandacardoso02@gmail.com>
2023-08-30 09:19:15 +0100

    Criando a controller de product com Find All e FindById

---
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new
+                ArgumentNullException(nameof(productRepository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _productRepository.FindAll();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _productRepository.FindById(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+    }
+}
